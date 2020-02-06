fbeef34abf4e328d76ffcea0ae099754f82fdb9e
mkfryatt <mkf29@cam.ac.uk>
2020-02-06 16:52:47 +0000

    matches tab again

--- ui/test.js
+++ ui/test.js
@@ -10,9 +10,9 @@ function openTab(event, type) {
     var i, tabcontent, tablinks;
 
     if (type=="match") {
-      addMatches();
+      addMatchPreviews();
     } else {
-      removeMatches();
+      removeMatchPreviews();
     }
 
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -29,26 +29,60 @@ function openTab(event, type) {
     event.currentTarget.className += " active";
 }
 
-function addMatches() {
+function addMatchPreviews() {
   //TODO get list of matches
   var matches = [{"reason": "blah", "matches": 3, "id": 343}, {reason: "dfdsvf", matches: 23, "id": 45}];
+
+  removeMatchPreviews();
   matches.forEach(element => {
+
     var div = document.createElement("div");
     div.setAttribute("class", "match_preview");
-    var inside = "<p>Reason: " 
-    + element.reason 
-    + "</p><p>Matches: " 
-    + element.matches 
-    + "</p><button id=\"view_match\" onclick=\"viewMatch("
-    + element.id
-    +")\">View</button><button id=\"remove_match\" onclick=\"viewMatch("
-    + element.id
-    +")\">X</button>"
-    div.innerHTML = inside;
+    div.setAttribute("id", "match_preview_"+ element.id);
+
+    var p1 = document.createElement("p");
+    p1.innerHTML = "Reason: " + element.reason;
+    div.appendChild(p1);
+
+    var p2 = document.createElement("p");
+    p2.innerHTML = "Matches: " + element.matches;
+    div.appendChild(p2);
+
+    var button1 = document.createElement("button");
+    button1.innerHTML = "View";
+    button1.setAttribute("onclick", "viewMatch(" + element.id + ")");
+    div.appendChild(button1);
+
+    var button2 = document.createElement("button");
+    button2.innerHTML = "Remove";
+    button2.setAttribute("onclick", "removeMatch(" + element.id + ")");
+    div.appendChild(button2);
+
     document.getElementById("match").appendChild(div);
   });
 }
 
-function removeMatches() {
+function removeMatchPreviews() {
   document.getElementById("match").innerHTML = "<h3>View your matches</h3>";
+}
+
+function viewMatch(id) {
+  document.getElementById("match").innerHTML = "";
+
+  var div = document.createElement("div");
+  div.setAttribute("class", "match_view");
+  div.innerHTML = "<p>info about the match here<\p>";
+
+  var button = document.createElement("button");
+  button.innerHTML = "Back";
+  button.setAttribute("onclick", "addMatchPreviews()");
+  div.appendChild(button);
+  
+  document.getElementById("match").appendChild(div);
+}
+
+function removeMatch(id) {
+  //TODO alert box
+  //TODO tell db to delete it
+  document.getElementById("match").removeChild(document.getElementById("match_preview_"+id));
 }
