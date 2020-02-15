80661445b03abff672771d00d7cbd7644e19122b
lukecavabarrett <luca.cavallery@gmail.com>
2020-02-15 12:06:08 +0000

    Added interaction

--- ui/util.js
+++ ui/util.js
@@ -1,5 +1,19 @@
-function getUnepPresencesWithinTimeframe(start, end) {
-    //TODO:
+function getUnepPresencesWithinTimeframe(start, end, callbackFunction) {
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            callbackFunction(JSON.parse(this.responseText));
+        }
+    }
+    let request = {
+        method: 'getUnepPresencesWithinTimeframe',
+        params: {
+            start : start,
+            end : end
+        }
+    };
+    xmlhttp.open("GET", middleware_path + "util.php?q=" + JSON.stringify(request), true);
+    xmlhttp.send();
 }
 
 function getOrganizationPresencesWithinTimeframe(organization, start, end) {
