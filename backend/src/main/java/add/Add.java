fdd167fd4d24e7dd7d4ecee36ed0508c85457db6
K-Shah3 <50206580+K-Shah3@users.noreply.github.com>
2020-02-19 13:40:38 +0000

    Add with cases

--- backend/src/main/java/add/Add.java
+++ backend/src/main/java/add/Add.java
@@ -1,10 +1,23 @@
 package main.java.add;
+import database.DatabaseConnector;
+
 
 public class Add {
+    public static DatabaseConnector dbCon = new DatabaseConnector("database.db");
+
     public static void add(String table, int key) {
         switch(table) {
             case "UNEP HQ":
                 AddedAggregatedUnepPresence.add(table, key);
+            case "UNEP Trip":
+                AddedAggregatedUnepPresence.add(table, key);
+            case "Organisation HQ":
+                AddedAggregatedOrgPresence.add(table, key);
+            case "Wish":
+                AddedAggregatedWish.add(table, key);
+
+
+
 
         }
     }
