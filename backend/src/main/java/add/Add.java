3409a2b933666b0d72005994d8aa1e2df3516bac
K-Shah3 <50206580+K-Shah3@users.noreply.github.com>
2020-02-19 13:09:39 +0000

    Add

---
+++ backend/src/main/java/add/Add.java
@@ -0,0 +1,13 @@
+public class Add(){
+    public static void add(String table,int key) {
+        switch(table) {
+            case "UNEP HQ":
+            case "UNEP Trip":
+                addFromAgrrUNEP();
+                break;
+            case "Location Wish":
+
+        }
+    }
+
+}
