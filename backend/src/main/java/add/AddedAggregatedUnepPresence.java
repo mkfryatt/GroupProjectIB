fbc7d7a4f4d5a90d18030cfa11e5afce67034104
lukecavabarrett <luca.cavallery@gmail.com>
2020-02-24 11:14:45 +0000

    cleaned AddedAggregatedUnepPresence.java

--- backend/src/main/java/add/AddedAggregatedUnepPresence.java
+++ backend/src/main/java/add/AddedAggregatedUnepPresence.java
@@ -7,16 +7,6 @@ import cost.Cost;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-//For each wish:
-//does it have any Organization Constraints?
-// No, it doesn't
-// It HAS to be one (and only one) location constraints.
-// Consider cost of the union of the time constraints of the wish and the timeframe (possibly infinte) of the newly added uneppresence. use this to generate 1 suggestion.
-// Yes it does
-// query in AggregatedOrgPresences where org is one of the OrgConstraints associated with the wish, and also filter for a possible location constraints (if no location constraints, don't filter by location)
-// For each OrgPresence, consider the intersection between timeframe of that OrgPresence and the union of the wish timefrmaes; for each interval generate 1 suggestion.
-
-
 public class AddedAggregatedUnepPresence {
 
 
