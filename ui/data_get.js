2cec48e106e51425b4714e898d71401b8403cf2a
mkfryatt <mkf29@cam.ac.uk>
2020-02-24 01:35:56 +0000

    add footer

--- ui/data_get.js
+++ ui/data_get.js
@@ -1,5 +1,13 @@
 function getAllTravel() {
-  return [{"city":"cambridge", "country":"uk", "startDate": "2020-06-06", "endDate": "2020-07-06", "id":1}];
+  return [{"city":"cambridge", "country":"uk", "startDate": "2020-06-06", "endDate": "2020-07-06", "id":1},
+  {"city":"cambridge", "country":"uk", "startDate": "2020-06-06", "endDate": "2020-07-06", "id":1},
+  {"city":"cambridge", "country":"uk", "startDate": "2020-06-06", "endDate": "2020-07-06", "id":1},
+  {"city":"cambridge", "country":"uk", "startDate": "2020-06-06", "endDate": "2020-07-06", "id":1},
+  {"city":"cambridge", "country":"uk", "startDate": "2020-06-06", "endDate": "2020-07-06", "id":1},
+  {"city":"cambridge", "country":"uk", "startDate": "2020-06-06", "endDate": "2020-07-06", "id":1},
+  {"city":"cambridge", "country":"uk", "startDate": "2020-06-06", "endDate": "2020-07-06", "id":1},
+  {"city":"cambridge", "country":"uk", "startDate": "2020-06-06", "endDate": "2020-07-06", "id":1},
+  {"city":"cambridge", "country":"uk", "startDate": "2020-06-06", "endDate": "2020-07-06", "id":1}];
 }
 
 function getTravelByID(id) {
@@ -10,8 +18,12 @@ function getMatchPreviews() {
   return [{"reason": "reason goes here", "matches": 3, "id": 1},
   {"reason": "reason goes here", "matches": 4, "id": 2},
   {"reason": "blah", "matches": 3, "id": 3}, 
-  {"reason": "reason goes here", "matches": 3, "id": 4}, 
-  {"reason": "reason goes here", "matches": 3, "id": 5}];
+  {"reason": "reason goes here", "matches": 1, "id": 4}, 
+  {"reason": "ht", "matches": 5, "id": 5},
+  {"reason": "dfhgbf", "matches": 3, "id": 6},
+  {"reason": "dfgf", "matches": 2, "id": 8},
+  {"reason": "kuhjm", "matches": 1, "id": 9},
+  {"reason": "dhfdb", "matches": 5, "id": 10}];
 }
 
 function getMatchDetails(id) {
