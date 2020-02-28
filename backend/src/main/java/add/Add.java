5d8d48adb26160115e756c427d507dd79ebd9297
K-Shah3 <50206580+K-Shah3@users.noreply.github.com>
2020-02-28 16:41:31 +0000

    Recompiled jar file Added tests

--- backend/src/main/java/add/Add.java
+++ backend/src/main/java/add/Add.java
@@ -6,16 +6,17 @@ import java.sql.SQLException;
 
 
 public class Add {
-    // TODO CHANGE FILEPATH
-//    public static DatabaseConnector dbCon = new DatabaseConnector("C:\\Users\\keval\\Documents\\Cambridge\\Part " +
-//            "1B\\Group Project\\Juliet\\backend\\database.db");
+
     public static DatabaseConnector dbCon;
 
-    public Add(String database_path) {
-        dbCon = new DatabaseConnector(database_path);
+    public static void setDbCon(String databasePath) {
+        dbCon = new DatabaseConnector(databasePath);
     }
 
     public static void add(String table, int key) throws SQLException {
+        if(dbCon == null) {
+            throw new InternalError("path to database has not been initialised");
+        }
         switch (table) {
             case "unep_presences":
             case "trips":
@@ -29,19 +30,20 @@ public class Add {
                 AddedAggregatedWish.add(key);
                 break;
             default:
-                System.out.println("Unknown table string. Pls fix.");
+                System.out.println("Table name " + table + " is unknown");
         }
     }
 
     public static void main(String[] args) throws SQLException{
         // Expecting 3 commmandline arguments - database path, table, key
+        if(args.length != 3) {
+            throw new IllegalArgumentException("Incorrect number of arguments supplied");
+        }
         String databasePath = args[0];
         String table = args[1];
         int key = Integer.parseInt(args[2]);
-        Add adder = new Add(databasePath);
-        adder.add(table, key);
-
-
+        setDbCon(databasePath);
+        add(table, key);
 
     }
 }
--- backend/src/test/java/AddingToDatabaseTests.java
+++ backend/src/test/java/AddingToDatabaseTests.java
@@ -1,5 +1,28 @@
 package test.java;
 
+import main.java.add.Add;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 public class AddingToDatabaseTests {
+
+    @Test
+    public static void checkErrorThrownIfPathToDbNotInitialised(String[] args) {
+        try {
+            Add.add(args[1], Integer.parseInt(args[2]));
+            Assertions.fail();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Assertions.fail();
+        } catch (InternalError e) {
+            Assertions.assertEquals(e.getMessage(), "path to database has not been initialised");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkErrorThrownIfPathToDbNotInitialised(args);
+    }
 
 }
